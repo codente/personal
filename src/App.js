a3e7d1e8186cfccefe36d906d33e3224da14d660
Jamie <jmeleacc@gmail.com>
2025-02-12 19:50:29 -0600

    Delete index.php as its undeeded

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+//import './App.css';
+
+function App() {
+  
+
+
+
+  
+  
+ 
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+       JamieVanRaalte.com
+      </header>
+      
+      
+     
+     
+    </div>
+  );
+}
+
+export default App;
